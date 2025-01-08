ae8e25e3e372b5ec2a1e48d61d77ea6a068a5b43
Mike Muir <mike@muirsandwich.com>
2025-01-07 17:30:42 -0800

    muircosmos

---
+++ keyboards/handwired/muircosmos/rules.mk
@@ -0,0 +1,16 @@
+# Build Options
+#   change yes to no to disable
+
+AUDIO_SUPPORTED = no
+RGB_MATRIX_SUPPORTED = yes
+RGBLIGHT_SUPPORTED = yes
+BOOTMAGIC_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+SPLIT_KEYBOARD = yes
+SERIAL_DRIVER = vendor
+VIA_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+
+# Trackball
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = pmw3389 # Change to pmw3360 if you use that
